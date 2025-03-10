79e2878938d23372b7ff972f29bc58b594b94b55
José Pedro <166343703+JP18090@users.noreply.github.com>
2025-03-10 11:09:12 +0000

    #

---
+++ Prog II/Prog II/tarefa10032025/GerenciadorNomesBD.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class GerenciadorNomesBD implements GerenciadorNomes {
+
+    @Override
+    public List<String> obterNomes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obterNomes'");
+    }
+
+    @Override
+    public void adicionarNome(String nome) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarNome'");
+    }
+
+}
